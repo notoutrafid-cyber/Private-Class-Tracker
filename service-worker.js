2d5fbcf56fac7ad4d186c83cda7042691a0536cf
notoutrafid-cyber <notoutrafid@gmail.com>
2026-09-11 01:27:39 +0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,52 @@
+const CACHE_NAME = "private-class-tracker-v1";
+
+const FILES_TO_CACHE = [
+    "./",
+    "./index.html",
+    "./manifest.json"
+];
+
+
+self.addEventListener("install", function(event){
+
+    event.waitUntil(
+
+        caches.open(CACHE_NAME)
+
+        .then(function(cache){
+
+            return cache.addAll(FILES_TO_CACHE);
+
+        })
+
+    );
+
+});
+
+
+self.addEventListener("activate", function(event){
+
+    event.waitUntil(
+
+        self.clients.claim()
+
+    );
+
+});
+
+
+self.addEventListener("fetch", function(event){
+
+    event.respondWith(
+
+        caches.match(event.request)
+
+        .then(function(response){
+
+            return response || fetch(event.request);
+
+        })
+
+    );
+
+});
